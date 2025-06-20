27e6e1b09630c8d0134f386d6cda9320173bbd47
m2a5x6i0m1 <max25601new@gmail.com>
2025-06-20 13:42:12 +0300

    ...

--- nixos/modules/desktop.nix
+++ nixos/modules/desktop.nix
@@ -5,12 +5,29 @@
   services.hypridle.enable = true;
 
   # Ly login manager
-  services.displayManager.ly.enable = true;
+  # services.displayManager.ly.enable = true;
 
   # UWSM
   # programs.uwsm.enable = true;
   # programs.hyprland.withUWSM = true;
   # programs.uwsm.waylandCompositors = {};
+
+  # Autologin with Greetd
+  services = {
+    greetd = {
+      enable = true;
+      settings = {
+        initial_session = {
+          command = "Hyprland";
+          user = "${"max"}";
+        };
+        default_session = {
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --greeting 'Welcome To NixOS' --asterisks --remember --remember-user-session --time --cmd Hyprland";
+          user = "greeter";
+        };
+      };
+    };
+  };
 
   # Status bar
   programs.waybar.enable = true;
